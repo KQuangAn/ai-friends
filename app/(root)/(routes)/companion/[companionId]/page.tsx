67819d5acc6f0e140ec311ce9e26bc7d4d03fa8d
KQuangAn <khuatquangan88@gmail.com>
2023-11-06 15:01:24 +0700

    add companion form

---
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -0,0 +1,21 @@
+import prismadb from "@/lib/prismadb";
+
+interface CompanionPageProps {
+  params: {
+    companionId: string;
+  };
+}
+
+const CompanionPage = async ({ params }: CompanionPageProps) => {
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  });
+
+  const categories = await prismadb.category.findMany();
+
+  return <;
+};
+
+export default CompanionPage;
