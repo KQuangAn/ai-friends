63490e59957d6cab03ea2d122c59c9ccfac72afa
KQuangAn <khuatquangan88@gmail.com>
2023-11-07 23:10:02 +0700

    add chat page

---
+++ app/(chat)/layout.tsx
@@ -0,0 +1,7 @@
+const ChatLayout = ({children}:children React.reactNode) => {
+    return ( 
+        <div className="mx-auto max-w-4xl "></div>
+     );
+}
+ 
+export default ChatLayout;
--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -2,10 +2,10 @@ import { auth, currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
-import { checkSubscription } from "@/lib/subscription";
+//import { checkSubscription } from "@/lib/subscription";
 
 export async function PATCH(
-  req: Request,
+  req: Request,  
   { params }: { params: { companionId: string } }
 ) {
   try {
@@ -25,11 +25,11 @@ export async function PATCH(
       return new NextResponse("Missing required fields", { status: 400 });
     };
 
-    const isPro = await checkSubscription();
+    // const isPro = await checkSubscription();
 
-    if (!isPro) {
-      return new NextResponse("Pro subscription required", { status: 403 });
-    }
+    // if (!isPro) {
+    //   return new NextResponse("Pro subscription required", { status: 403 });
+    // }
 
     const companion = await prismadb.companion.update({
       where: {
